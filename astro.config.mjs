bd0addeb4a894fafe336bab9f43ad2fffdc6379a
Dipankar <dipankar.db37@gmail.com>
2026-06-14 12:32:43 +0530

    fix wrangler and astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-
+import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+    imageService: "passthrough", // disables Cloudflare Images binding
+    sessionKVNamespaceName: undefined, // disables KV session binding
+  }),
   vite: {
     plugins: [tailwindcss()],
   },
