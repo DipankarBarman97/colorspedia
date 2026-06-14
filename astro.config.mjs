eee8453253a552ab507840fa4bb15949ec405194
Dipankar <dipankar.db37@gmail.com>
2026-06-14 12:50:34 +0530

    fix global and remove @astrojs/cloudflare package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,8 @@
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   output: "server",
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-    imageService: "passthrough", // disables Cloudflare Images binding
-    sessionKVNamespaceName: undefined, // disables KV session binding
-  }),
   vite: {
     plugins: [tailwindcss()],
   },
